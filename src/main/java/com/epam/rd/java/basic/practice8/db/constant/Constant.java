49551b88820a2f838d2577c60453a3320334986c
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-08-24 22:38:04 +0300

    overrided equals for Team and User 6

--- src/main/java/com/epam/rd/java/basic/practice8/db/constant/Constant.java
+++ src/main/java/com/epam/rd/java/basic/practice8/db/constant/Constant.java
@@ -18,8 +18,8 @@ public class Constant {
 
     public static final String SET_TEAMS_FOR_USER = "INSERT INTO users_teams(teams_id,users_id) VALUES(?,?)";
     public static final String GET_TEAMS_FROM_USER = "SELECT teams.id, teams.name FROM users_teams" +
-            " INNER JOIN teams ON users_teams.teams_id = teams.id" +
-            " INNER JOIN users ON users_teams.users_id = users.id" +
+            " INNER JOIN teams ON users_teams.team_id = teams.id" +
+            " INNER JOIN users ON users_teams.user_id = users.id" +
             " WHERE users.login = ?";
 
     public static final String DELETE_TEAM = "DELETE FROM teams WHERE id = ?";
