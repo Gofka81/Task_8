969b11b736f9c441c36a5d30f16b9d3bd24394b0
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-08-25 15:34:56 +0300

    added 5 tests 5

--- src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
+++ src/test/java/com/epam/rd/java/basic/practice8/Part4StudentTest.java
@@ -20,5 +20,77 @@ import java.util.List;
 import java.util.Properties;
 
 public class Part4StudentTest {
+    private static final String JDBC_DRIVER = "org.h2.Driver";
+    private static final String DB_URL = "jdbc:h2:~/test";
+    private static final String URL_CONNECTION = "jdbc:h2:~/test;user=youruser;password=yourpassword;";
+    private static final String USER = "youruser";
+    private static final String PASS = "yourpassword";
 
+    private static DBManager dbManager;
+    private static Connection connection;
+
+    @BeforeClass
+    public static void beforeTest(){
+
+        try (OutputStream output = new FileOutputStream("app.properties")) {
+            Properties prop = new Properties();
+            prop.setProperty("connection.url", URL_CONNECTION);
+            prop.store(output, null);
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+        dbManager = DBManager.getInstance();
+
+        try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement statement = con.createStatement()) {
+            connection = con;
+            String sql = "CREATE TABLE IF NOT EXISTS teams (\n" +
+                    "  id INTEGER(11) NOT NULL AUTO_INCREMENT,\n" +
+                    " name VARCHAR(20) NOT NULL, \n" +
+                    "  PRIMARY KEY (id));" +
+                    "\nCREATE TABLE IF NOT EXISTS users (\n"+
+                    " id INTEGER(11) NOT NULL AUTO_INCREMENT,\n"+
+                    " login VARCHAR(20) NOT NULL, \n "+
+                    " PRIMARY KEY (id));\n" +
+                    "CREATE TABLE users_teams (\n" +
+                    "user_id INT REFERENCES users(id) ON DELETE CASCADE,\n" +
+                    "team_id INT REFERENCES teams(id) ON DELETE CASCADE,\n" +
+                    "UNIQUE (user_id, team_id));";
+
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        dbManager.insertUser(User.createUser("ivanov"));
+        dbManager.insertUser(User.createUser("teamA"));
+        dbManager.insertUser(User.createUser("teamB"));
+    }
+
+    @AfterClass
+    public static void tearDownDB(){
+        try (Connection con = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement statement = con.createStatement()) {
+            connection = con;
+            String sql = "DROP TABLE IF EXISTS teams; \n" +
+                    "DROP TABLE IF EXISTS users; \n" +
+                    "DROP TABLE IF EXISTS users_teams;";
+
+            statement.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    @Test
+    public void shoudDeleteTeam(){
+        User user = dbManager.getUser("ivanov");
+        Team team1 = dbManager.getTeam("teamA");
+        Team team2 = dbManager.getTeam("teamB");
+        dbManager.setTeamsForUser(user,team1,team2);
+        List<Team> teams1 = dbManager.getUserTeams(user);
+        dbManager.deleteTeam(team2);
+        Assert.assertEquals(teams1.get(0).getName(), team1.getName());
+    }
 }
