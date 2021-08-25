44c6e6a8a1fe47a6f5da4f50c01b5ffa141495fa
Liubomyr Antonyk <liubchyk.antonyk@gmail.com>
2021-08-25 20:28:35 +0300

    added 5 tests 14

--- src/main/java/com/epam/rd/java/basic/practice8/Demo.java
+++ src/main/java/com/epam/rd/java/basic/practice8/Demo.java
@@ -1,11 +1,6 @@
 package com.epam.rd.java.basic.practice8;
 
-
-import com.epam.rd.java.basic.practice8.db.entity.User;
-
 public class Demo {
-
-
     public static void main(String[] args) {
         //demo
     }
